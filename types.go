1b26f1102dd2d6b754ec05cada9b823cdd3e43d8
Evaldas Leliuga <evaldas.leliuga@gmail.com>
2023-08-31 15:25:57 +0300

    Added model interface

--- types.go
+++ types.go
@@ -3,4 +3,10 @@ package data
 type (
 	// Kind defines a data kind.
 	Kind uint8
+
+	// IModel defines a model interface.
+	IModel interface {
+		// Validate makes `Model` validatable by implementing [validation.Validatable] interface.
+		Validate() error
+	}
 )
